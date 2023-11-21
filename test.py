bba0073ce3d49b0bb1a031cf7f49e9add74f3309
AravindReddyGuda <1234aravindreddy@gmail.com>
2023-11-21 23:20:17 +0000

    Yt Downloader push

--- test.py
+++ test.py
@@ -1,3 +1,25 @@
+import numpy as np
+import scipy.misc as sc
+from sklearn import datasets 
+from sklearn import metrics 
+from sklearn.tree import DecisionTreeClassifier 
+# import theano 
+# import theano.tensor as T 
+# import tensorflow as tf
+# import keras as ke
+# import pytorch as pt
+import pandas as pd
+import matplotlib as mp
+
+
+
+
+
+
+
+
+
+
 
 def fibonacci(n, num1, num2, next_number, count):
     while count <= n:
